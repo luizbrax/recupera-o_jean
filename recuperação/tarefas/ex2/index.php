dbfa26d2758db212880072cf1f9a7bd5bff5f7c5
luizbrax <167213921+luizbrax@users.noreply.github.com>
2025-07-24 17:40:10 -0300

    Add files via upload

--- recuperação/tarefas/ex2/index.php
+++ recuperação/tarefas/ex2/index.php
@@ -4,16 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ex1</title>
+    <title>dados do servidor</title>
 </head>
 
 <body>
-    <h1>
-        <?php
-        echo "Olá Mundo \u{1F30E}";
-        ?>
-    </h1>
-    <P>PARAGUAI</P>
+    <h1>dados do servidor</h1>
+    <?php
+    phpinfo();
+    ?>
 </body>
 
 </html>
