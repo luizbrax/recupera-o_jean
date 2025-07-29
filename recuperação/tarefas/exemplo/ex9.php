9a80bff2db268015eff233835c4740f21426019c
luizbrax <167213921+luizbrax@users.noreply.github.com>
2025-07-29 19:02:08 -0300

    Add files via upload

---
+++ recuperação/tarefas/exemplo/ex9.php
@@ -0,0 +1,12 @@
+<?php 
+$canal = "curso em video";
+$ano = date('Y');
+echo <<< TESTE
+    ola galerinha do canal!
+        tudo bem com vcs?
+            como esta sendo a ferias de vcs?
+    espero q esteja sendo bom:)
+
+TESTE;
+
+?>
